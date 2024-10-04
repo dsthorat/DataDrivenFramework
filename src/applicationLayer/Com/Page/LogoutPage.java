44b8d88639ee7e2f73d9135a4373711a8685b9ff
sai <sai@DESKTOP-TG4UPQG>
2024-10-04 23:39:19 +0530

    First Commit

---
+++ src/applicationLayer/Com/Page/LogoutPage.java
@@ -0,0 +1,16 @@
+package applicationLayer.Com.Page;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LogoutPage {
+	
+	@FindBy(xpath = "(//a[starts-with(text(),' Logout')])[2]")
+	WebElement ObjLogout;
+	public void adminLogout()
+	{
+		ObjLogout.click();
+	}
+
+
+}
